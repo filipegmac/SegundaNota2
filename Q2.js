1316cf6320b7a7194a162e5bb687f37a25336328
zScorpion <felipemacielgamer777@gmail.com>
2024-10-08 13:33:43 -0300

    Segunda atividade da segunda nota

--- Q2.js
+++ Q2.js
@@ -2,8 +2,8 @@ function somar(numero){
     let soma = 0;
     for (let i = 1; i <= numero; i++) {
         soma += i
-        console.log(soma);
     }
+    return console.log(soma);
 }
 
 somar(5)
---
+++ Q3.js
@@ -0,0 +1,10 @@
+function idade(idade){
+    if (idade < 18){
+        return console.log("Menor de idade")
+    } else {
+        return console.log("Maior de idade")
+    }
+}
+
+idade(17)
+idade(18)
